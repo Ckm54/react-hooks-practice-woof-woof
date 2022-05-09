b743bb1803e1a2b730a91bf114d0a513ef8d9b82
Collins <mutaic84@gmail.com>
2022-05-09 12:42:02 +0300

    display each dogs info

--- src/components/DogInfo.js
+++ src/components/DogInfo.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-function DogInfo() {
+function DogInfo({ dog }) {
+  console.log(dog)
   return (
     <div id="dog-summary-container">
         <h1>DOGGO:</h1>
-        <div id="dog-info"></div>
+        <div id="dog-info">
+          <img src={dog.image} alt={dog.name} />
+          <h2>{dog.name}</h2>
+          <button>{dog.isGoodDog ? "Good Dog!" : "Bad Dog!"}</button>
+        </div>
       </div>
   )
 }
