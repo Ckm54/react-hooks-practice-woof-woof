414ef8c44b0718b6ea7ba7c268de1c45cdac2641
Collins <mutaic84@gmail.com>
2022-05-09 12:30:20 +0300

    padd down dog data

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import React from "react"
 import Filter from "./Filter";
 import DogBar from "./DogBar";
 
-function Header() {
+function Header({ dogs }) {
   return (
     <>
       <Filter />
-      <DogBar />
+      <DogBar dogs={dogs}/>
     </>
   )
 }
