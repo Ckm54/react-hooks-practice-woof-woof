f8637891dac22702fd99e4313e934c54f46e0a6c
Collins <mutaic84@gmail.com>
2022-05-09 12:24:56 +0300

    create a header component to house header content

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Filter from "./Filter";
+import DogBar from "./DogBar";
+
+function Header() {
+  return (
+    <>
+      <Filter />
+      <DogBar />
+    </>
+  )
+}
+
+export default Header
