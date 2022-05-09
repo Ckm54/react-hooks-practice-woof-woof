7f67bce6073a51304cd8be6c31ce376ab805233d
Collins <mutaic84@gmail.com>
2022-05-09 15:43:27 +0300

    create a handler to toggle between good and bad dog

--- src/components/DogInfo.js
+++ src/components/DogInfo.js
@@ -1,13 +1,17 @@
 import React from "react";
 
-function DogInfo({ dog }) {
+function DogInfo({ dog, handleGoodness }) {
+  function handleDog() {
+    
+    handleGoodness(dog)
+  }
   return (
     <div id="dog-summary-container">
         <h1>DOGGO:</h1>
         <div id="dog-info">
           <img src={dog.image} alt={dog.name} />
           <h2>{dog.name}</h2>
-          <button>{dog.isGoodDog ? "Good Dog!" : "Bad Dog!"}</button>
+          <button onClick={handleDog}>{dog.isGoodDog ? "Good Dog!" : "Bad Dog!"}</button>
         </div>
       </div>
   )
