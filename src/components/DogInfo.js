1fca3c7ff6e5b08e18e1aac9b54b8f9fc18b4ac1
Collins <mutaic84@gmail.com>
2022-05-09 12:23:49 +0300

    create a component to display a dogs info

---
+++ src/components/DogInfo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function DogInfo() {
+  return (
+    <div id="dog-summary-container">
+        <h1>DOGGO:</h1>
+        <div id="dog-info"></div>
+      </div>
+  )
+}
+
+export default DogInfo
