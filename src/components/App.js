44450042646ff2444f5badf30a6ec887b4b02e96
Collins <mutaic84@gmail.com>
2022-05-09 12:22:45 +0300

    split into components

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Header from "./Header";
+import DogInfo from "./DogInfo";
 
 function App() {
+  const [dogs, setDogs] = useState([])
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/pups`)
+    
+  })
   return (
     <div className="App">
-      <div id="filter-div">
-        <button id="good-dog-filter">Filter good dogs: OFF</button>
-      </div>
-      <div id="dog-bar"></div>
-      <div id="dog-summary-container">
-        <h1>DOGGO:</h1>
-        <div id="dog-info"></div>
-      </div>
+      <Header />
+      <DogInfo />
     </div>
   );
 }
