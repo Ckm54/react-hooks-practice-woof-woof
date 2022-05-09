eb48da00020716f8947e143ab152a544c03b13c9
Collins <mutaic84@gmail.com>
2022-05-09 12:37:38 +0300

    create function to handle passing new dog data to info component

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,21 @@ import DogInfo from "./DogInfo";
 
 function App() {
   const [dogs, setDogs] = useState([])
+  const [dog, setDog] = useState({})
 
   useEffect(() => {
     fetch(`http://localhost:3001/pups`)
     .then((res) => res.json())
     .then((data) => setDogs(data))
-  }, [])
+  }, []);
+
+  function handleDisplayInfo(dog) {
+    setDog(dog)
+  }
   return (
     <div className="App">
-      <Header dogs={dogs}/>
-      <DogInfo />
+      <Header dogs={dogs} displayInfo={handleDisplayInfo}/>
+      <DogInfo dog={dog}/>
     </div>
   );
 }
