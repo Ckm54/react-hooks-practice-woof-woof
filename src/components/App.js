9235ebbe45570d94c8c61282f4ff544b7e3c3bac
Collins <mutaic84@gmail.com>
2022-05-09 12:48:27 +0300

    display first dog on info container when page loads

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,15 @@ import DogInfo from "./DogInfo";
 
 function App() {
   const [dogs, setDogs] = useState([])
-  const [dog, setDog] = useState({})
+  const [dog, setDog] = useState(dogs[0])
 
   useEffect(() => {
     fetch(`http://localhost:3001/pups`)
     .then((res) => res.json())
-    .then((data) => setDogs(data))
+    .then((data) => {
+      setDogs(data)
+      setDog(data[0])
+    })
   }, []);
 
   function handleDisplayInfo(dog) {
@@ -18,7 +21,7 @@ function App() {
   return (
     <div className="App">
       <Header dogs={dogs} displayInfo={handleDisplayInfo}/>
-      <DogInfo dog={dog}/>
+      {dog ? <DogInfo dog={dog}/> : null}
     </div>
   );
 }
