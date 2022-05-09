1242318c3b2007dad7ad46b456ee2a473892da87
Collins <mutaic84@gmail.com>
2022-05-09 15:44:11 +0300

    handle changing and updating a dog's goodness

--- src/components/App.js
+++ src/components/App.js
@@ -18,10 +18,35 @@ function App() {
   function handleDisplayInfo(dog) {
     setDog(dog)
   }
+
+  function toggleDogGoodness(dog){
+    fetch(`http://localhost:3001/pups/${dog.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        ...dog,
+        isGoodDog: !dog.isGoodDog
+      })
+    })
+    .then((res) => res.json())
+    .then((editedDog) => {
+      setDog(editedDog)
+      const newData = dogs.map((dog) => {
+        if(dog.id === editedDog.id) {
+          return editedDog
+        } else{
+          return dog
+        }
+      })
+      setDogs(newData)
+    })
+  }
   return (
     <div className="App">
-      <Header dogs={dogs} displayInfo={handleDisplayInfo}/>
-      {dog ? <DogInfo dog={dog}/> : null}
+      <Header dogs={dogs} displayInfo={handleDisplayInfo} />
+      {dog ? <DogInfo dog={dog} handleGoodness={toggleDogGoodness}/> : null}
     </div>
   );
 }
